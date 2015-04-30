d28777e7d8571d373e7b01b3b203cdb4c64b6bd4
padma <padmavb@thoughtworks.com>
2015-04-30 14:31:50 +0530

    updating upload back up amazon s3 script

--- upload_backup_to_s3.sh
+++ upload_backup_to_s3.sh
@@ -1,2 +1,3 @@
-s3cmd put /home/centos/ajabshahar_backup_$(date +"%A").sql s3://singaporeajabshaharbackup/backup_folder/
-s3cmd put /home/centos/ajabshahar_images_$(date +"%A").zip s3://singaporeajabshaharbackup/ajabshahar_images/
+mkdir $(date +"%A")
+s3cmd put /home/centos/ajabshahar_backup_$(date +"%A").sql s3://singaporeajabshaharbackup/backup_folder/$(date + "%A")
+s3cmd put /home/centos/ajabshahar_images_$(date +"%A").zip s3://singaporeajabshaharbackup/backup_folder/$(date + "%A")
