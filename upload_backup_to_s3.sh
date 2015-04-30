b4b6329961c4fcbf9542bc12ce6f6ec5b1dd5ce9
padma <padmavb@thoughtworks.com>
2015-04-30 15:20:46 +0530

    Reverting to previous commit

--- upload_backup_to_s3.sh
+++ upload_backup_to_s3.sh
@@ -1,3 +1,2 @@
-mkdir $(date +"%A")
-s3cmd put /home/centos/ajabshahar_backup_$(date +"%A").sql s3://singaporeajabshaharbackup/backup_folder/$(date + "%A")
-s3cmd put /home/centos/ajabshahar_images_$(date +"%A").zip s3://singaporeajabshaharbackup/backup_folder/$(date + "%A")
+s3cmd put /home/centos/ajabshahar_backup_$(date +"%A").sql s3://singaporeajabshaharbackup/backup_folder/
+s3cmd put /home/centos/ajabshahar_images_$(date +"%A").zip s3://singaporeajabshaharbackup/ajabshahar_images/
