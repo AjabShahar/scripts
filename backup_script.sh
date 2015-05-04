60d0ffe8b939b4ad37292e16f2c915d516e7c467
padma <padmavb@thoughtworks.com>
2015-05-04 15:29:41 +0530

    Corrected path issues in backup script

--- backup_script.sh
+++ backup_script.sh
@@ -1,2 +1,2 @@
-pg_dump "platform-dev" -U "postgres" > /home/centos/backup_folder/ajabshahar_backup_$(date +"%A").sql
-zip /home/centos/ajabshahr_images/"ajabshahar_images_$(date +"%A").zip" -j  /var/www/images/*
+pg_dump "platform-dev" -U "postgres" > /home/centos/ajabshahar_backup_$(date +"%A").sql
+zip /home/centos/"ajabshahar_images_$(date +"%A").zip" -j  /var/www/images/*
